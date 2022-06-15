1c8817b64553bd161e6f6d4b8c3fc8cd1254ae0b
Giulia <giuliafloris88@gmail.com>
2022-06-15 12:53:54 +0200

    Initial Commit

---
+++ PlayAnimalsSound/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayAnimalsSound
+//
+//  Created by user on 15/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
